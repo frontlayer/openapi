c36ed5fe5ef450007a134c4279971f112ffed509
Alexander Gavazov <alex@studio.bg>
2019-08-10 15:16:27 +0300

    fix absolute path include

--- tests/run.php
+++ tests/run.php
@@ -3,5 +3,5 @@ declare(strict_types=1);
 
 require __DIR__ . './../vendor/autoload.php';
 
-require './path.php';
-require './spec.php';
+require __DIR__ . '/path.php';
+require __DIR__ . '/spec.php';
